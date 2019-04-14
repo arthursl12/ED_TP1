a2fd638d8f0413a644748c11ba230ded70f085a6
arthursl12 <42850885+arthursl12@users.noreply.github.com>
2019-04-14 08:28:53 -0300

    Commit inicial
    - Implementações muito iniciais, testando a entrada e saída

---
+++ teste.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<cstdio>
+
+int main(int argc, char* argv[]){
+    int a = 2;
+    printf("Digite: ");
+    scanf("%i",&a);
+    printf("\nO numero foi: %i",a);
+    return 0;
+}
+
